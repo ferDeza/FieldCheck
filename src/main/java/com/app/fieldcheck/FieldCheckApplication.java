90855b6cb9ff557f3e2c25173e567e4d76498888
ferDeza <fernandodezasotomayor@gmail.com>
2026-02-11 20:47:08 -0500

    Upload project

---
+++ src/main/java/com/app/fieldcheck/FieldCheckApplication.java
@@ -0,0 +1,13 @@
+package com.app.fieldcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/fieldcheck/FieldCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fieldcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
