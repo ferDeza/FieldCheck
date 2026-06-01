5943754ea999f34ae229d66926c6ca3ce246b152
ferDeza <fernandodezasotomayor@gmail.com>
2026-06-01 18:22:51 -0500

    refactor: adding new allowed urls

--- backend/src/main/java/com/app/fieldcheck/config/SecurityConfig.java
+++ backend/src/main/java/com/app/fieldcheck/config/SecurityConfig.java
@@ -70,7 +70,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:5173",          // El puerto por defecto si usas Vite de forma local
+                "https://*.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
